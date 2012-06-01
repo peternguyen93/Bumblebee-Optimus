a835816733518f3a4f20a3a764919b44bc450a81
Peter Nguyen <peternguyen199321@gmail.com>
2012-06-01 20:16:12 +0700

    third commit

--- uninstall.sh
+++ uninstall.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+echo "Checking optimus..."
+sleep 3
 if [ -f "/usr/bin/optimus" ]; then
     echo "Removing......"
     sudo rm -f /usr/bin/optimus
     sudo rm -f /usr/share/applications/bumblebee-optimus.desktop
     sudo rm -f /usr/share/icons/optimus.png
+    echo "Done."
 else
-    echo "Optimus isn't installed"
+    echo "Optimus isn\'t installed"
 fi
