5aeb426b3ff8b588570f9a28a06ca3fb7d661753
Peter Nguyen <peternguyen199321@gmail.com>
2012-06-01 20:16:03 +0700

    third commit

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-echo "Checking bumblebee"
+echo "Checking bumblebee....."
 sleep 3
 if [ -f "/usr/bin/optirun" ]; then
     echo "Bumblebee was installed"
     sleep 1
-    echo "Install bumblebee optimus"
+    echo "Install bumblebee optimus....."
     sudo cp -Rv optimus.py /usr/bin/optimus
     sudo chmod o+x /usr/bin/optimus
     sudo cp -Rv bumblebee-optimus.desktop /usr/share/applications/
     sudo cp -Rv optimus.png /usr/share/icons
-    
+    sleep 1
+    echo "Done."
 else
     echo "Bumblebee isn't installed. Please install now."
 fi
