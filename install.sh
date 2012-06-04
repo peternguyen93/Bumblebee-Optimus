c574166ae50689323d10fbe436fc6827a7741642
Peter Nguyen <peternguyen199321@gmail.com>
2012-06-04 09:35:02 +0700

    fourth commit update code installation

--- install.sh
+++ install.sh
@@ -5,7 +5,11 @@ sleep 3
 if [ -f "/usr/bin/optirun" ]; then
     echo "Bumblebee was installed"
     sleep 1
-    echo "Install bumblebee optimus....."
+    if [ -f "/usr/bin/optimus" ]; then
+	echo "Update bumblebee optimus...."
+    else
+	echo "Install bumblebee optimus....."
+    fi
     sudo cp -Rv optimus.py /usr/bin/optimus
     sudo chmod o+x /usr/bin/optimus
     sudo cp -Rv bumblebee-optimus.desktop /usr/share/applications/
